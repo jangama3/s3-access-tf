0566eaa5790ec686e1bb94c42c5a6a9cdf18bf78
gabousmen <gabousmen@hotmail.com>
2024-04-24 13:10:48 -0400

    sssssss

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "javpc2" {
 resource "aws_subnet" "sub1" {
   vpc_id     = aws_vpc.javpc2.id
   cidr_block = "10.0.0.0/24"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
 
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "sub1" {
 resource "aws_subnet" "sub2" {
   vpc_id     = aws_vpc.javpc2.id
   cidr_block = "10.0.1.0/24"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
 
  
