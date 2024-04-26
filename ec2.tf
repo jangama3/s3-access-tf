371d2643d49465b7c0d7437887655a68d87eb4cf
gabousmen <gabousmen@hotmail.com>
2024-04-26 18:30:23 -0400

    dddd

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "webserver1" {
-  name = "webserver1"
+ 
   ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
   vpc_security_group_ids = [
@@ -7,7 +7,9 @@ resource "aws_instance" "webserver1" {
   subnet_id = aws_subnet.sub1.id
   user_data = base64encode("userdata1.sh")
 
- 
+  tags = {
+    name = "webserver1"
+  }
 }
 
 resource "aws_instance" "webserver2" {
@@ -18,6 +20,9 @@ resource "aws_instance" "webserver2" {
     aws_security_group.websg.id]
   subnet_id = aws_subnet.sub2.id
   user_data = base64encode("userdata2.sh")
+   tags = {
+    name = "webserver12
+  }
 
  
 }
