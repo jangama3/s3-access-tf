f1b450dfc6d3d8a24992437b774e969123ed3150
gabousmen <gabousmen@hotmail.com>
2024-04-26 13:54:54 -0400

    sssss

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "webserver1" {
   vpc_security_group_ids = [
     aws_security_group.websg.id]
   subnet_id = aws_subnet.sub1.id
-  user_data = "${file("apache_config.sh")}"
+  user_data = "${file("userdata1.sh")}"
 
  
 }
