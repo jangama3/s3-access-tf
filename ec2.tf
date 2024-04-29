72059de5c6cacc177e0f7327ee99e8e2144e7ed6
gabousmen <gabousmen@hotmail.com>
2024-04-29 12:14:56 -0400

    NEW EC2

--- ec2.tf
+++ ec2.tf
@@ -5,19 +5,10 @@ resource "aws_instance" "webserver1" {
   vpc_security_group_ids = [
     aws_security_group.websg.id]
   subnet_id = aws_subnet.sub1.id
-  user_data = <<-EOF
-   #!/bin/bash
-   sudo apt update
-   sudo apt install apache2 -y
-   echo "Hello from Srini" > /var/www/html/index.html
-   echo "Hi there !!"> /var/www/html/hi.html
-   echo "test webserver" > /var/www/html/test.html
-   sudo systemctl restart apache2
-   sudo systemctl enable apache2
-  EOF  
+  user_data = user_data = file("userdata1.sh")
 
   tags = {
-    name = "webserver1"
+    Name = "webserver1"
   }
 }
 
@@ -32,7 +23,7 @@ resource "aws_instance" "webserver2" {
   #useruser_data = "${file('userdata2.sh')}"
   #user_data = base64encode("userdata2.sh")
    tags = {
-    name = "webserver2"
+    Name = "webserver2"
   }
 
  
