30d31459efdc56002e08baf22c7a278104cc813e
gabousmen <gabousmen@hotmail.com>
2024-04-29 07:44:44 -0400

    wserw

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,14 @@ resource "aws_instance" "webserver1" {
   vpc_security_group_ids = [
     aws_security_group.websg.id]
   subnet_id = aws_subnet.sub1.id
-  user_data = base64encode("userdata1.sh")
+  user_data = << EOF
+		#! /bin/bash
+                sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
 
   tags = {
     name = "webserver1"
